f3ec067c6859ba23118d3970d86be74c51381f51
jinhyo <kjhyo0220@gmail.com>
2025-03-28 15:53:28 +0900

    ✨ Feat: 첫 컴포넌트 만들어보기 #3
    chapter 02 - 첫 컴포넌트 만들어보기 실습

--- week2-1/src/App.tsx
+++ week2-1/src/App.tsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import List from "./components/List";
 
 function App() {
+  const nickname = "지니";
+  const sweetPotato = "고구마";
+  const array = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
   return (
     <>
-      <h1>Hello React</h1>
+      <strong className="school">덕성여자대학교</strong>
+      <p style={{ color: "purple", fontWeight: "bold", fontSize: "3rem" }}>{nickname}/김진효</p>
+      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
+      <ul>
+        {array.map((yaho, idx) => (
+          <List key={idx} tech={yaho} />
+        ))}
+      </ul>
     </>
   );
 }
