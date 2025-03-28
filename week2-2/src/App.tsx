c260aebaca76236e68f609b572a03bca4df5aa94
jinhyo <kjhyo0220@gmail.com>
2025-03-28 16:58:10 +0900

    ✨ Feat: useState 기초 #3
    useState 기초 실습 진행

--- week2-2/src/App.tsx
+++ week2-2/src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncreaseNumber = () => {
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <>
-      <h1>React</h1>
+      <h1>{count}</h1>
+      <button onClick={handleIncreaseNumber}>숫자 증가</button>
     </>
   );
 }
