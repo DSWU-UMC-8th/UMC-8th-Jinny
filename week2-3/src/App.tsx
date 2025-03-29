8b2c8ed84ab3e4b501bef2cfe7ead843d252d00c
jinhyo <kjhyo0220@gmail.com>
2025-03-29 15:15:30 +0900

    ✨ Feat: 카운터 기능 추가 #3
    1 증가 및 감소 기능 추가

--- week2-3/src/App.tsx
+++ week2-3/src/App.tsx
@@ -1,29 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 
-function heavyComputation(): number {
-  let result = 0;
-  for (let i = 0; i < 1_000_000; i++) {
-    result += 1;
-  }
-  return result;
-}
+// function heavyComputation(): number {
+//   let result = 0;
+//   for (let i = 0; i < 1_000_000; i++) {
+//     result += 1;
+//   }
+//   return result;
+// }
 
 function App() {
-  const [count, setCount] = useState(heavyComputation);
+  const [count, setCount] = useState(0);
   // const [count, setCount] = useState(() => heavyComputation());
 
-  console.log(heavyComputation());
-
-  const handleIncrease = (): void => {
-    setCount((prev): number => prev + 1);
+  const handleValue = (value: number): void => {
+    setCount((prev): number => prev + value);
 
     console.log(count);
   };
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={handleIncrease}>증가</button>
+      <button onClick={() => handleValue(+1)}>증가</button>
+      <button onClick={() => handleValue(-1)}>감소</button>
     </>
   );
 }
