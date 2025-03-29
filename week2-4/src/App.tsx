da656f023b831073649fa21e82a5246322b8fe9c
jinhyo <kjhyo0220@gmail.com>
2025-03-29 15:54:39 +0900

    ✨ Feat: useContext 실습 #3
    useContext 실습

--- week2-4/src/App.tsx
+++ week2-4/src/App.tsx
@@ -1,21 +1,16 @@
-import { useState } from "react";
+import { useContext } from "react";
 import ButtonGroup from "./components/ButtonGroup";
+import { CounterContext } from "./context/CounterProvider";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const context = useContext(CounterContext); // CounterProvider에서 전달한 값 가져옴
 
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrement = () => {
-    setCount(count - 1);
-  };
+  console.log(context);
 
   return (
     <>
-      <h1>{count}</h1>
-      <ButtonGroup handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
+      <h1>{context?.count}</h1>
+      <ButtonGroup handleIncrement={context?.handleIncrement} handleDecrement={context?.handleDecrement} />
     </>
   );
 }
