7b213214137093782c41492455d0684292bd7f6b
jinhyo <kjhyo0220@gmail.com>
2025-04-01 16:02:13 +0900

    ✨ Feat: 라우팅 설정 #5
    React Rounter 기본 사용법 실습

--- week3-1/src/App.tsx
+++ week3-1/src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>🏠홈 페이지</h1>,
+  },
+  {
+    path: "/movies",
+    element: <h1>🍿영화 페이지</h1>,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
