2adf903590674b5805290a40b4a9906be2d70546
jinhyo <kjhyo0220@gmail.com>
2025-04-02 12:53:37 +0900

    ✨ Feat: clean up function #5
    clean up function 실습

--- week3-2/src/App.tsx
+++ week3-2/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import MoviesPage from "./components/MoviesPage";
+// import MoviesPage from "./components/MoviesPage";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   return (
     <>
-      <MoviesPage />
+      {/* <MoviesPage /> */}
+      <SearchPage />
     </>
   );
 }
