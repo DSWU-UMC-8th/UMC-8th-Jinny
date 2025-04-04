59a23f58c74b3aab9d5652e453f64532b516a120
jinhyo <kjhyo0220@gmail.com>
2025-04-05 03:08:27 +0900

    ✨ Feat: 페이지네이션 및 로딩 연결 #5
    페이지 이동 및 로딩 효과 뜨도록

--- week3-3/src/pages/MoviePage.tsx
+++ week3-3/src/pages/MoviePage.tsx
@@ -13,13 +13,16 @@ const MoviePage = () => {
   //2. 에러 상태
   const [isError, setIsError] = useState(false);
 
+  //3. 페이지
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
     const fetchMovies = async () => {
       setIsPending(true);
 
       try {
         const { data } = await axios.get<MovieResponse>(
-          `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
+          `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=${page}`,
           {
             headers: {
               Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -36,7 +39,7 @@ const MoviePage = () => {
     };
 
     fetchMovies();
-  }, []);
+  }, [page]);
 
   // if (!isPending) {
   //   return <LoadingSpinner />;
@@ -52,11 +55,32 @@ const MoviePage = () => {
 
   return (
     <>
-      <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      <div className="flex items-center justify-center gap-6 mt-5">
+        <button
+          className="bg-[#228be6] text-white px-5 py-2 rounded-lg shadow-md hover:bg-[#1a68ad] transition-all duration-200 disabled:bg-gray-300 cursor-pointer disabled:cursor-not-allowed"
+          disabled={page === 1}
+          onClick={() => setPage((prev) => prev - 1)}
+        >{`<`}</button>
+        <span>{page}페이지</span>
+        <button
+          className="bg-[#228be6] text-white px-5 py-2 rounded-lg shadow-md hover:bg-[#1a68ad] transition-all duration-200 cursor-pointer"
+          onClick={() => setPage((prev) => prev + 1)}
+        >{`>`}</button>
       </div>
+
+      {isPending && (
+        <div className="flex items-center justify-center h-dvh">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {!isPending && (
+        <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
