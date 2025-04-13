81246e21a1a12111f0502927cc314276202d3162
jinhyo <kjhyo0220@gmail.com>
2025-04-13 15:31:41 +0900

    ✨ Feat: 비밀번호 보이게 버튼 추가 #7
    비밀번호 보이게 / 안 보이게

--- week4/src/pages/SignupPage.tsx
+++ week4/src/pages/SignupPage.tsx
@@ -3,7 +3,9 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { postSignup } from "../apis/auth";
 import google from "../assets/img/google.png";
-import { useNavigate } from "react-router-dom";
+import mailIcon from "../assets/img/email.png";
+import view from "../assets/img/view.png";
+import close from "../assets/img/eye.png";
 import { useState } from "react";
 
 const schema = z
@@ -36,13 +38,15 @@ type FormFields = z.infer<typeof schema>;
 
 const SignUpPage = () => {
   const [step, setStep] = useState(1); // 1단계: 이메일, 2단계: 비밀번호, 3단계: 이름
+  const [seePass, setSeePass] = useState(false);
+  const [seePassCheck, setSeePassCheck] = useState(false);
 
   const {
     register,
     handleSubmit,
     trigger,
     watch,
-    formState: { errors, isSubmitted },
+    formState: { errors },
   } = useForm<FormFields>({
     defaultValues: {
       name: "",
@@ -70,36 +74,53 @@ const SignUpPage = () => {
     if (valid) setStep((prev) => prev + 1);
   };
 
-  const navigate = useNavigate();
-
   const email = watch("email");
   const emailValid = !errors.email && email !== "";
+
   const password = watch("password");
   const passwordCheck = watch("passwordCheck");
+  const passValid = !errors.password && !errors.passwordCheck && password != "" && passwordCheck !== "";
+
   const name = watch("name");
+  const nameValid = !errors.name && name !== "";
+
+  const toggleSeePass = () => {
+    setSeePass((prev) => !prev);
+  };
+
+  const toggleSeePassCheck = () => {
+    setSeePassCheck((prev) => !prev);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center gap-4 h-[calc(100dvh_-_56px)]">
       <div className="flex flex-row w-[300px] items-center">
-        <h3 className="text-2xl cursor-pointer" onClick={() => navigate(-1)}>{`<`}</h3>
+        <h3
+          className="text-2xl cursor-pointer"
+          onClick={() =>
+            setStep((prev) => {
+              if (prev !== 1) return prev - 1;
+              return prev;
+            })
+          }
+        >{`<`}</h3>
         <h3 className="text-2xl font-bold flex-1 text-center">회원가입</h3>
-      </div>
-
-      <div className="border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm flex flex-row p-4 items-center cursor-pointer">
-        <img src={google} alt="구글 로고" className="w-[30px] h-[30px]"></img>
-        <p className="flex-1 text-center">구글 로그인</p>
-      </div>
-
-      <div className="flex flex-row items-center justify-between w-[300px]">
-        <div className="w-[100px] h-[2px] bg-black"></div>
-        <p className="font-bold">OR</p>
-        <div className="w-[100px] h-[2px] bg-black"></div>
       </div>
 
       <div className="flex flex-col gap-3">
         {/* 1단계: 이메일 */}
         {step === 1 && (
           <>
+            <div className="border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm flex flex-row p-4 items-center cursor-pointer">
+              <img src={google} alt="구글 로고" className="w-[30px] h-[30px]"></img>
+              <p className="flex-1 text-center">구글 로그인</p>
+            </div>
+
+            <div className="flex flex-row items-center justify-between w-[300px]">
+              <div className="w-[100px] h-[2px] bg-black"></div>
+              <p className="font-bold">OR</p>
+              <div className="w-[100px] h-[2px] bg-black"></div>
+            </div>
             <input
               {...register("email")}
               className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
@@ -114,7 +135,9 @@ const SignUpPage = () => {
               onClick={handleNext}
               disabled={!emailValid}
               className={`w-full py-3 rounded-md text-lg font-medium transition-colors ${
-                !emailValid ? "bg-gray-300 text-white cursor-not-allowed" : "bg-blue-600 text-white hover:bg-blue-700"
+                !emailValid
+                  ? "bg-gray-300 text-white cursor-not-allowed"
+                  : "bg-blue-600 text-white hover:bg-blue-700 cursor-pointer"
               }`}
             >
               다음
@@ -125,30 +148,57 @@ const SignUpPage = () => {
         {/* 2단계: 비밀번호 */}
         {step === 2 && (
           <>
-            <input
-              {...register("password")}
-              className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
-                errors?.password ? "border-red-500 bg-red-200" : "border-gray-300"
-              }`}
-              type={"password"}
-              placeholder={"비밀번호"}
-            />
+            <div className="flex gap-4 items-center m-2">
+              <img src={mailIcon} alt="이메일" className="w-[20px] h-[20px]"></img>
+              <p>{email}</p>
+            </div>
+            <div className="flex relative items-center">
+              <input
+                {...register("password")}
+                className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
+                  errors?.password ? "border-red-500 bg-red-200" : "border-gray-300"
+                }`}
+                type={seePass ? "text" : "password"}
+                placeholder={"비밀번호"}
+              />
+              <img
+                src={seePass ? view : close}
+                alt="눈 표시"
+                onClick={toggleSeePass}
+                className="w-[20px] h-[20px] cursor-pointer absolute right-5"
+              ></img>
+            </div>
+
             {errors.password && <div className="text-red-500 text-sm">{errors.password.message}</div>}
 
-            <input
-              {...register("passwordCheck")}
-              className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
-                errors?.passwordCheck ? "border-red-500 bg-red-200" : "border-gray-300"
-              }`}
-              type={"password"}
-              placeholder={"비밀번호 확인"}
-            />
+            <div className="flex relative items-center">
+              <input
+                {...register("passwordCheck")}
+                className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm ${
+                  errors?.passwordCheck ? "border-red-500 bg-red-200" : "border-gray-300"
+                }`}
+                type={seePassCheck ? "text" : "password"}
+                placeholder={"비밀번호 확인"}
+              />
+              <img
+                src={seePassCheck ? view : close}
+                alt="눈 표시"
+                onClick={toggleSeePassCheck}
+                className="w-[20px] h-[20px] cursor-pointer absolute right-5"
+              ></img>
+            </div>
+
             {errors.passwordCheck && <div className="text-red-500 text-sm">{errors.passwordCheck.message}</div>}
 
             <button
               type="button"
               onClick={handleNext}
-              className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors"
+              disabled={!passValid}
+              className={`w-full py-3 rounded-md text-lg font-medium transition-colors ${
+                !passValid
+                  ? "bg-gray-300 text-white cursor-not-allowed"
+                  : "bg-blue-600 text-white hover:bg-blue-700 cursor-pointer"
+              }`}
             >
               다음
             </button>
@@ -171,8 +221,12 @@ const SignUpPage = () => {
             <button
               type="button"
               onClick={handleSubmit(onSubmit)}
-              disabled={isSubmitted}
-              className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
+              disabled={!nameValid}
+              className={`w-full py-3 rounded-md text-lg font-medium transition-colors ${
+                !nameValid
+                  ? "bg-gray-300 text-white cursor-not-allowed"
+                  : "bg-blue-600 text-white hover:bg-blue-700 cursor-pointer"
+              }`}
             >
               회원가입
             </button>
