27d0b33cc2a5b471d0dca481258cb6accb32e3cc
jinhyo <kjhyo0220@gmail.com>
2025-05-31 17:30:09 +0900

    ✨ Feat: useReducer 실습 #18
    useReducer 실습

--- week9/src/App.tsx
+++ week9/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import UseReducerCompany from "./pages/UseReducerCompany";
+import UseReducerPage from "./pages/useReducerPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <UseReducerPage />
+      <hr />
+      <UseReducerCompany />
+    </>
+  );
 }
 
 export default App;
